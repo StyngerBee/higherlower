3cc9af0b008720b4bd93d6cb42a3edd301ff3db8
Curran Duke <cdsonics@yahoo.com>
2022-10-14 11:30:02 -0400

    added top 200 tracks

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,7 +6,7 @@ const options = {
 	}
 };
 
-fetch('https://spotify81.p.rapidapi.com/top_20_by_monthly_listeners', options)
+fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
