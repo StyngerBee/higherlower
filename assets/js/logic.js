917dc7613da0a5368f169cf80fe1c516ba7256c0
Curran Duke <cdsonics@yahoo.com>
2022-10-14 11:22:19 -0400

    added rapidapi for spotify popularity

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,34 +1,12 @@
-var request = require('request'); // "Request" library
-
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-
-// your application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7e06928a00msh707867a2a351098p112c16jsn62fbfdebcd93',
+		'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+	}
 };
 
-request.post(authOptions, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // use the access token to access the Spotify Web API
-    var token = body.access_token;
-    var options = {
-      url: 'https://api.spotify.com/v1/users/jmperezperez',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      },
-      json: true
-    };
-    request.get(options, function(error, response, body) {
-      console.log(body);
-    });
-  }
-});
+fetch('https://spotify81.p.rapidapi.com/top_20_by_monthly_listeners', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
