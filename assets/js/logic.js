011a0230628c531be9c60e06b9972ecf65d4d140
Curran Duke <cdsonics@yahoo.com>
2022-10-14 14:15:04 -0400

    added api for top 200 songs

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -10,3 +11,5 @@ fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
